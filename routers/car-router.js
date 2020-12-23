d85402ea5236bb5b12556dc43396f85041e93841
dwainejade <dwainejade@gmail.com>
2020-12-22 19:48:25 -0500

    get car by ID

--- routers/car-router.js
+++ routers/car-router.js
@@ -3,31 +3,34 @@ const db = require("../data/connect")
 const router = express.Router()
 
 // GET LIST OF CARS
-router.get("/cars", async (req, res, next) =>{
+router.get("/cars", async (req, res, next) => {
     try {
-		res.json(await db("car-dealer"))
-	} catch(err) {
-		next(err)
-	}
+        res.json(await db("car-dealer"))
+    } catch (err) {
+        next(err)
+    }
 })
 
 // GET CAR BY ID
-router.get("/cars/id", async (req, res, next) =>{
+router.get("/cars/:id", async (req, res, next) => {
     try {
-		res.json(await db("car-dealer"))
-	} catch(err) {
-		next(err)
-	}
+        const { id } = req.params
+        const car = await db("car-dealer").where({ id }).first()
+
+        res.json(car)
+    } catch (err) {
+        next(err)
+    }
 })
 
 // POST CAR
-router.post("/cars", (req, res, next) =>{
-    
+router.post("/cars", (req, res, next) => {
+
 })
 
 // DELETE CAR
-router.delete("/cars/:id", (req, res, next) =>{
-    
+router.delete("/cars/:id", (req, res, next) => {
+
 })
 
 module.exports = router
